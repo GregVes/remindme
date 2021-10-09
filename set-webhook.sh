b01538ecf1ff40fc215ad29f6fe389cbd4dd3c16
Gregory Vespasien <gregory@packer-output-0ec6c79a-d7c9-401e-bf1a-a65337e4af8b>
2021-10-09 09:34:47 +0200

    remove delete webhook script

--- set-webhook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TELEGRAM_TOKEN=${TELEGRAM_BOT_TOKEN}
-CLOUD_FUNCTION_URL="https://radio4000-dev-api.space/bot"
-
-curl --data "url=$CLOUD_FUNCTION_URL" https://api.telegram.org/bot$TELEGRAM_TOKEN/SetWebhook
