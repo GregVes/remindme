8c47c10027050d45036e2925be03387884840159
Gregory Vespasien <gregory.vespasien@curalie.com>
2021-10-24 13:08:56 +0200

    add connect to db before creating db in init script

--- db/01-init.sh
+++ db/01-init.sh
@@ -6,6 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE USER $REMINDME_DB_USER WITH PASSWORD '$REMINDME_DB_PASSWORD';
   CREATE DATABASE $REMINDME_DB_NAME;
   GRANT ALL PRIVILEGES ON DATABASE $REMINDME_DB_NAME TO $REMINDME_DB_USER;
+  \connect remindme;
   CREATE TABLE IF NOT EXISTS reminder (
     id serial PRIMARY KEY,
     chat_id INT NOT NULL,
@@ -17,7 +18,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     annual_date VARCHAR(30) DEFAULT NULL,
     unique_date date,
     unique_time time NOT NULL,
-    CONSTRAINT valid_month_date CHECK (monthly_day BETWEEN 1 AND 31),
-    UNIQUE (chat_id, chat_message)
-  )
+    CONSTRAINT valid_month_date CHECK (monthly_day BETWEEN 1 AND 31)
+  );
 EOSQL
