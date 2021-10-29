f8340790d659c54ab087d3dbf8f73fb5f4ee2fb4
Gregory Vespasien <gregory.vespasien@curalie.com>
2021-10-29 18:00:50 +0200

    remove unused files

--- post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-curl -X POST -H "Content-Type: application/json" -d @data.json https://gregentoo.com/bot
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t server .
-
-sudo docker run -e TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN} server
