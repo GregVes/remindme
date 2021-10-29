b8e0313af1a86775979ca98476c221ccdb944cac
Gregory Vespasien <gregory.vespasien@curalie.com>
2021-10-29 17:38:12 +0200

    allow all times

--- db/migration/000003_reminder_dummy_data.up.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO reminder (chat_id, chat_message, is_recurrent, is_everyday, unique_time) VALUES (11, 'do this', true, true, '0:00');
-INSERT INTO reminder (chat_id, chat_message, is_recurrent, weekly_day, unique_time) VALUES (11, 'do this', true, 'Thursday', '0:00');
-INSERT INTO reminder (chat_id, chat_message, is_recurrent, monthly_day, unique_time) VALUES (11, 'do this', true, 26, '0:00');
-INSERT INTO reminder (chat_id, chat_message, is_recurrent, annual_date, unique_time) VALUES (11, 'do this', true, '2021-10-28', '0:00');
-INSERT INTO reminder (chat_id, chat_message, unique_date, unique_time) VALUES (11, 'do this', '2021-10-28', '0:00');
